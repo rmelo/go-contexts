331764c77b74bb57ac3f2dd26fc9ffa912417740
Rodrigo Melo <rdg.melo@gmail.com>
2020-10-30 03:26:52 -0300

    Changes

--- context-with-cancel/main.go
+++ context-with-cancel/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"math/rand"
 	"time"
 )
 
@@ -12,9 +13,10 @@ func main() {
 	defer cancel()
 
 	f := func(n int, ctx context.Context) {
+		rand.Seed(time.Now().UnixNano())
 		fmt.Printf("func %v started with %v sec\n", n, n)
 		select {
-		case <-time.After(time.Second * time.Duration(n)):
+		case <-time.After(time.Second * time.Duration(rand.Intn(5))):
 			fmt.Printf("func %v win!\t\t[ok]\n", n)
 			cancel()
 		case <-ctx.Done():
--- examples/server/main.go
+++ examples/server/main.go
@@ -4,7 +4,11 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
@@ -12,13 +16,32 @@ func main() {
 	port := flag.String("p", "3000", "server port")
 	flag.Parse()
 
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%v", *port),
+	}
+
 	fmt.Printf("\n\nMagic happens on http://localhost:%v\n\n", *port)
 
 	http.HandleFunc("/order", order)
 	http.HandleFunc("/payment", payment)
 	http.HandleFunc("/antifraud", antifraud)
 
-	http.ListenAndServe(fmt.Sprintf(":%v", *port), nil)
+	go func() {
+		srv.ListenAndServe()
+	}()
+
+	quit := make(chan os.Signal)
+
+	signal.Notify(quit, os.Interrupt)
+
+	<-quit
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	srv.Shutdown(ctx)
+	log.Println("shutting down")
+	os.Exit(0)
 }
 
 func get(ctx context.Context, endpoint string) error {
